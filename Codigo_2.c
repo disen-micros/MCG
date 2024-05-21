efb5f6b0a8f8efdb90883fd6daaf54cf9a10d497
disen-micros <84601596+disen-micros@users.noreply.github.com>
2024-05-21 10:54:40 -0600

    Add files via upload

---
+++ Codigo_2.c
@@ -0,0 +1,62 @@
+
+#include <stdio.h>
+#include "MK64F12.h"
+#include "fsl_clock.h"
+#include "FlexTimer.h"
+
+
+#define MCG_IRCLK_DISABLE 0U                   /*!< MCGIRCLK disabled */
+#define MCG_PLL_DISABLE 0U                     /*!< MCGPLLCLK disabled */
+#define OSC_CAP0P 0U                           /*!< Oscillator 0pF capacitor load */
+#define OSC_ER_CLK_DISABLE 0U                  /*!< Disable external reference clock */
+#define SIM_CLKOUT_SEL_FLEXBUS_CLK 0U          /*!< CLKOUT pin clock select: FlexBus clock */
+#define SIM_ENET_1588T_CLK_SEL_OSCERCLK_CLK 2U /*!< SDHC clock select: OSCERCLK clock */
+#define SIM_ENET_RMII_CLK_SEL_EXTAL_CLK 0U     /*!< SDHC clock select: Core/system clock */
+#define SIM_OSC32KSEL_RTC32KCLK_CLK 2U         /*!< OSC32KSEL select: RTC32KCLK clock (32.768kHz) */
+#define SIM_PLLFLLSEL_IRC48MCLK_CLK 3U         /*!< PLLFLL select: IRC48MCLK clock */
+#define SIM_PLLFLLSEL_MCGPLLCLK_CLK 1U         /*!< PLLFLL select: MCGPLLCLK clock */
+#define SIM_SDHC_CLK_SEL_OSCERCLK_CLK 2U       /*!< SDHC clock select: OSCERCLK clock */
+#define SIM_TRACE_CLK_SEL_CORE_SYSTEM_CLK 1U   /*!< Trace clock select: Core/system clock */
+#define SIM_USB_CLK_120000000HZ 120000000U     /*!< Input SIM frequency for USB: 120000000Hz */
+
+int main(void) {
+
+	 mcg_pll_config_t 	 pll0Config =
+		 	        {
+		 	            .enableMode = MCG_PLL_DISABLE, /* MCGPLLCLK disabled */
+		 	            .prdiv      = 0xeU,            /* PLL Reference divider: divided by 15 */
+		 	            .vdiv       = 0xCU,            /* VCO divider: multiplied by 36 */
+		 	        };
+
+	const osc_config_t osc_config = {
+	    .freq        = 50000000U,    /* Oscillator frequency: 50000000Hz */
+	    .capLoad     = (OSC_CAP0P),  /* Oscillator capacity load: 0pF */
+	    .workMode    = kOSC_ModeExt, /* Use external clock */
+	    .oscerConfig = {
+	        .enableMode =
+	            kOSC_ErClkEnable, /* Enable external reference clock, disable external reference clock in STOP mode */
+	    }};
+
+
+	CLOCK_SetSimSafeDivs();
+
+	CLOCK_InitOsc0(&osc_config);
+	CLOCK_SetXtal0Freq(osc_config.freq);
+
+
+	CLOCK_BootToPeeMode(kMCG_OscselOsc, kMCG_PllClkSelPll0, &pll0Config);
+
+
+	SystemCoreClock = CLOCK_GetFreq(kCLOCK_CoreSysClk);
+
+	SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
+	PORTC->PCR[1]   = PORT_PCR_MUX(0x4);
+
+
+	FlexTimer_Init();
+
+    while(1) {
+
+    }
+    return 0 ;
+}
---
+++ FlexTimer.c
@@ -0,0 +1,36 @@
+/**
+	\file
+	\brief
+		This is the starter file of FlexTimer.
+		In this file the FlexTimer is configured in overflow mode.
+	\author J. Luis Pizano Escalante, luispizano@iteso.mx
+	\date	21/03/2019
+	\todo
+	    Add configuration structures.
+ */
+
+
+
+#include "FlexTimer.h"
+#include "MK64F12.h"
+
+
+
+void FlexTimer_Init(void)
+{
+	/**Clock gating for FlexTimer*/
+	SIM->SCGC6 |= SIM_SCGC6_FTM0(1);
+	/**It enable the FTM*/
+	FTM0->MODE |= FTM_MODE_FTMEN_MASK;
+	/**Selects the FTM behavior in BDM mode.In this case in functional mode*/
+	FTM0->CONF |= FTM_CONF_BDMMODE(3);
+	/**Assign modulo register with a predefined value*/
+	FTM0->MOD = 0x01;
+	/**Configure FlexTimer in output compare in toggle mode*/
+	FTM0->CONTROLS[0].CnSC = FLEX_TIMER_MSA | FLEX_TIMER_ELSA;
+	/**Assign channel value register with a predefined value*/
+	FTM0->CONTROLS[0].CnV = 0x01;
+	/**Select clock source and prescaler*/
+	FTM0->SC |= FTM_SC_CLKS (FLEX_TIMER_CLKS_1)| FTM_SC_PS(FLEX_TIMER_PS_128);
+}
+
